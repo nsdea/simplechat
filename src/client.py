683e3aa808a167b7b6607c6321a3effc765946da
Felix | onlix <neostyxde@gmail.com>
2021-09-04 23:59:49 +0200

    good night
    Co-authored-by: 404-exe <404-exe@users.noreply.github.com>

--- src/client.py
+++ src/client.py
@@ -1,74 +1,126 @@
 #!/usr/bin/env python3
-'''Script for Tkinter GUI chat client.'''
+"""Script for Tkinter GUI chat client."""
 from socket import AF_INET, socket, SOCK_STREAM
 from threading import Thread
 
 import tkinter
+import tkinter.messagebox
 import webbrowser
-import pyautogui
+
+messages_sent_count = 0
+
+def display(text):
+    global chat_display
+    
+    chat_display.config(state='normal')
+    chat_display.tag_config('author', font=('Consolas', 20, 'bold'))
+    
+    chat_display.insert('end', text.split()[0] + ' ', 'author')
+    chat_display.insert('end', ' '.join(text.split()[1:]) + '\n')
+    chat_display.see('end')
+    chat_display.config(state='disabled')
 
 def receive():
-    '''Handles receiving of messages.'''
+    """Handles receiving of messages."""
     while True:
         try:
             msg = client_socket.recv(BUFSIZ).decode('utf8')
-            msg_list.insert(tkinter.END, msg)
+            display(msg)
 
             if '<LINK>' in msg:
                 webbrowser.open(msg.split('<LINK>')[1])
-            if '<SCREEN>' in msg:
-                pyautogui.screenshot().save('screen.jpg')
 
         except OSError:  # Possibly client has left the chat.
             break
 
-
 def send(event=None):  # event is passed by binders.
-    '''Handles sending of messages.'''
-    msg = my_msg.get()
-    my_msg.set('')  # Clears input field.
+    """Handles sending of messages."""
+    global window
+    global messages_sent_count
+    
+    msg = message_var.get()
+    message_var.set('')  # Clears input field.
+    
     client_socket.send(bytes(msg, 'utf8'))
+
+    if not messages_sent_count:
+        window.title(msg)
+
     if msg == '{quit}':
         client_socket.close()
         window.quit()
 
+    messages_sent_count += 1
+
 def on_closing(event=None):
-    '''This function is to be called when the window is closed.'''
-    my_msg.set('{quit}')
+    """This function is to be called when the window is closed."""
+    message_var.set('{quit}')
     send()
 
 window = tkinter.Tk()
-window.geometry('600x400')
+window.geometry('800x500')
 window.config(background='#111111')
-window.title('Chatter')
+window.title('Login')
 
 messages_frame = tkinter.Frame(window, borderwidth=0, highlightthickness=0, bd=0)
-my_msg = tkinter.StringVar()  # For the messages to be sent.
+message_var = tkinter.StringVar()  # For the messages to be sent.
 scrollbar = tkinter.Scrollbar(messages_frame, borderwidth=0, highlightthickness=0, bd=0)  # To navigate through past messages.
 
 # Following will contain the messages.
-msg_list = tkinter.Listbox(messages_frame, height=15, width=50, yscrollcommand=scrollbar.set, bg='#111111', fg='green')
+chat_display = tkinter.Listbox(
+    messages_frame,
+    height=13,
+    width=50,
+    font=('Consolas', 20),
+    yscrollcommand=scrollbar.set,
+    xscrollcommand=scrollbar.set,
+    bg='#111111',
+    fg='green',
+    highlightcolor='black'
+)
+chat_display = tkinter.Text(
+    messages_frame,
+    height=13,
+    width=50,
+    font=('Consolas', 20),
+    yscrollcommand=scrollbar.set,
+    xscrollcommand=scrollbar.set,
+    bg='#111111',
+    fg='green',
+    highlightcolor='black',
+    relief='flat',
+)
+
 scrollbar.pack(side=tkinter.RIGHT, fill=tkinter.Y)
-msg_list.pack(side=tkinter.LEFT, fill=tkinter.BOTH)
-msg_list.pack()
+chat_display.pack(side=tkinter.LEFT, fill=tkinter.BOTH)
+chat_display.pack()
 
 messages_frame.pack()
 
 entry_field = tkinter.Entry(
     window,
-    textvariable=my_msg,
-    bg='#112211',
+    textvariable=message_var,
+    bg='#111F11',
     fg='green',
-    font=('Consolas', 20),
-    width='10',
+    font=('Consolas', 20, 'bold'),
+    width=10,
     relief='flat',
     cursor='xterm green',
     insertbackground='green',
 )
 entry_field.bind('<Return>', send)
-entry_field.pack()
+entry_field.pack(fill='x')
+entry_field.focus_set()
 
-send_button = tkinter.Button(window, text='Send', command=send, bg='#111111', fg='green')
+send_button = tkinter.Button(
+    window,
+    text='Send',
+    command=send,
+    bg='#111111',
+    fg='green',
+    width=10,
+    font=('Consolas', 20),
+)
 send_button.pack()
 
 window.protocol('WM_DELETE_WINDOW', on_closing)
@@ -76,6 +128,7 @@ window.protocol('WM_DELETE_WINDOW', on_closing)
 #----Now comes the sockets part----
 HOST = 'localhost'
 PORT = 33000
+
 if not PORT:
     PORT = 33000
 else:
@@ -85,8 +138,14 @@ BUFSIZ = 1024
 ADDR = (HOST, PORT)
 
 client_socket = socket(AF_INET, SOCK_STREAM)
-client_socket.connect(ADDR)
 
-receive_thread = Thread(target=receive)
-receive_thread.start()
-tkinter.mainloop()  # Starts GUI execution.
+try:
+    client_socket.connect(ADDR)
+
+except:
+    tkinter.messagebox.showerror(title='Server Error!', message='The server is currently unavailable.')
+
+else:
+    receive_thread = Thread(target=receive)
+    receive_thread.start()
+    tkinter.mainloop()  # Starts GUI execution.
--- src/server.py
+++ src/server.py
@@ -1,63 +1,82 @@
 #!/usr/bin/env python3
 """Server for multithreaded (asynchronous) chat application."""
-from socket import AF_INET, socket, SOCK_STREAM
+import os
+import socket
+import random
+import time
+
 from threading import Thread
+from typing import NoReturn
 
+def send(client, text):
+    client.send(bytes(text, 'utf8'))
 
-def accept_incoming_connections():
+def on_join_request():
     """Sets up handling for incoming clients."""
+    
     while True:
-        client, client_address = SERVER.accept()
-        print("%s:%s has connected." % client_address)
-        client.send(bytes("Greetings from the cave! Now type your name and press enter!", "utf8"))
+        client, client_address = server.accept()
+        print('%s:%s connected' % client_address)
+        send(client, '[SYSTEM] Type your name to join the chat.')
+        
         addresses[client] = client_address
         Thread(target=handle_client, args=(client,)).start()
 
-
 def handle_client(client):  # Takes client socket as argument.
     """Handles a single client connection."""
-
-    name = client.recv(BUFSIZ).decode("utf8")
-    welcome = 'Welcome %s! If you ever want to quit, type {quit} to exit.' % name
-    client.send(bytes(welcome, "utf8"))
-    msg = "%s has joined the chat!" % name
-    broadcast(bytes(msg, "utf8"))
+    
+    name = client.recv(BUFFER_SIZE).decode('utf8').replace(' ', '_')
+    if not name:
+        name = f'Guest{random.randint(1000, 9999)}'
+    
+    send(client, f'>>> Welcome to the chat, @{name}!')
+    broadcast(f'@{name} joined the chat!')
     clients[client] = name
 
     while True:
-        msg = client.recv(BUFSIZ)
-        if msg != bytes("{quit}", "utf8"):
-            broadcast(msg, name+": ")
+        msg = client.recv(BUFFER_SIZE)
+        
+        if msg != bytes('{quit}', 'utf8'):
+            broadcast(msg, '@' + name)
         else:
-            client.send(bytes("{quit}", "utf8"))
+            client.send(bytes('{quit}', 'utf8'))
             client.close()
             del clients[client]
-            broadcast(bytes("%s has left the chat." % name, "utf8"))
+            broadcast(f'{name} left the chat.')
             break
 
-
-def broadcast(msg, prefix=""):  # prefix is for name identification.
+def broadcast(msg, prefix='[SERVER] '):  # prefix is for name identification.
     """Broadcasts a message to all the clients."""
+    
+    # if isinstance(msg, str):
+    #     msg = bytes(msg, 'utf8')
 
     for sock in clients:
-        sock.send(bytes(prefix, "utf8")+msg)
+        sock.send(bytes(f'[{prefix}] ', 'utf8') + msg)
 
-        
 clients = {}
 addresses = {}
 
 HOST = ''
 PORT = 33000
-BUFSIZ = 1024
-ADDR = (HOST, PORT)
+BUFFER_SIZE = 1024
 
-SERVER = socket(AF_INET, SOCK_STREAM)
-SERVER.bind(ADDR)
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-if __name__ == "__main__":
-    SERVER.listen(5)
-    print("Waiting for connection...")
-    ACCEPT_THREAD = Thread(target=accept_incoming_connections)
-    ACCEPT_THREAD.start()
-    ACCEPT_THREAD.join()
-    SERVER.close()
+try:
+    server.bind((HOST, PORT))
+
+except Exception as e:
+    print('SERVER ERROR:', e)
+    os.system('pkill -f python.exe')
+    exit()
+
+if __name__ == '__main__':
+    server.listen(5)
+    
+    print('SERVER ONLINE')
+    
+    join_thread = Thread(target=on_join_request)
+    join_thread.start()
+    join_thread.join()
+    server.close()
